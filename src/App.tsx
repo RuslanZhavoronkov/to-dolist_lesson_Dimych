2d355da1314a692acab4c25e1da8f6636f0c089e
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-06-27 23:02:10 +0300

    Lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { TaskType, Todolist } from "./Todolist";
 
+export type FilterValuesType = "All" | "Completed" | "Active";
+
 function App() {
   //Создадим массивы задач
-  let tasks1: Array<TaskType> = [
-    { id: 1, title: "CSS", isDone: true },
-    { id: 2, title: "JS", isDone: false },
-    { id: 3, title: "React", isDone: true },
-  ];
+  let [tasks, setTasks] = useState<Array<TaskType>>([
+    { id: 1, title: "HTML&CSS", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "ReactJS", isDone: false },
+    { id: 4, title: "Redux", isDone: false },
+  ]);
 
-  let tasks2: Array<TaskType> = [
-    { id: 1, title: "Terminator", isDone: true },
-    { id: 2, title: "XXX", isDone: true },
-    { id: 3, title: "Jentments of fortune", isDone: true },
-  ];
+  let [filter, setFilter] = useState<FilterValuesType>("All");
+
+  //Функция обработчик нажатия кнопки "x"(логическая функция)
+  function removeTask(id: number) {
+    setTasks(tasks.filter((t) => t.id !== id));
+  }
+
+  //Функция обработчик нажатия кнопок "All", "Completed", "Active"(логическая функция)
+  function changeFilter(value: FilterValuesType) {
+    setFilter(value);
+  }
+
+  let tasksForTodolist = tasks;
+
+  if (filter === "Completed") {
+    tasksForTodolist = tasks.filter((t) => t.isDone === true);
+  }
+  if (filter === "Active") {
+    tasksForTodolist = tasks.filter((t) => t.isDone === false);
+  }
 
   return (
     <div className="App">
-      <Todolist title="What to learn" tasks={tasks1} />
-      <Todolist title="Movies" tasks={tasks2} />
+      <Todolist
+        title="What to learn"
+        tasks={tasksForTodolist}
+        removeTask={removeTask}
+        changeFilter={changeFilter}
+      />
     </div>
   );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FilterValuesType } from "./App";
 
 //Создадим тип объектов массивов task1, task2
 export type TaskType = {
@@ -9,7 +10,9 @@ export type TaskType = {
 
 type PropsType = {
   title: string;
-  tasks: Array<TaskType>; //Ждем массив объектов типа TaskType
+  tasks: Array<TaskType>;
+  removeTask: (id: number) => void;
+  changeFilter: (value: FilterValuesType) => void;
 };
 
 export function Todolist(props: PropsType) {
@@ -21,23 +24,20 @@ export function Todolist(props: PropsType) {
         <button>+</button>
       </div>
       <ul>
-        <li>
-          <input type="checkbox" checked={props.tasks[0].isDone} />
-          <span>{props.tasks[0].title}</span>
-        </li>
-        <li>
-          <input type="checkbox" checked={props.tasks[1].isDone} />
-          <span>{props.tasks[1].title}</span>
-        </li>
-        <li>
-          <input type="checkbox" checked={props.tasks[2].isDone} />
-          <span>{props.tasks[2].title}</span>
-        </li>
+        {props.tasks.map((t) => (
+          <li>
+            <input type="checkbox" checked={t.isDone} />
+            <span>{t.title}</span>
+            <button onClick={() => props.removeTask(t.id)}>x</button>
+          </li>
+        ))}
       </ul>
       <div>
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+        <button onClick={() => props.changeFilter("All")}>All</button>
+        <button onClick={() => props.changeFilter("Active")}>Active</button>
+        <button onClick={() => props.changeFilter("Completed")}>
+          Completed
+        </button>
       </div>
     </div>
   );
