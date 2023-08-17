0197066f5159cac879cdf051ff1918c8f2eff91a
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-08-17 18:54:32 +0300

    Lesson 9_finish

---
+++ src/state/todolists-tasks-reducer.test.ts
@@ -0,0 +1,25 @@
+import { TaskStateType, TodolistType } from "../App"
+import { taskReducer } from "./task-reduser "
+import { addTodolistAC, todolistsReducer } from "./todolists-reduser"
+
+test('ids should be equals', () => {
+
+    const startTaskState: TaskStateType = {}
+    const startTodolistState: Array<TodolistType> = []
+
+    const action = addTodolistAC('new todolist')
+    const resultStartTaskState = taskReducer(startTaskState, action)
+    const resultTodolistState = todolistsReducer(startTodolistState, action)
+
+    const keys = Object.keys(resultStartTaskState)
+
+    const idFromTask = keys[0]
+    const idFromTodolist = resultTodolistState[0].id
+
+
+    expect(keys.length).toBe(1)
+    expect(resultTodolistState[0]).toBeDefined()
+    expect(idFromTask).toBe(action.payload.id)
+    expect(idFromTodolist).toBe(action.payload.id)
+
+})
